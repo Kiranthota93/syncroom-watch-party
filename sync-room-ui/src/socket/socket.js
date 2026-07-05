03bf08d56ac288c55ddb0e81d88a8008a612fa35
Kiran-enmovil <156183308+Kiran-enmovil@users.noreply.github.com>
2026-07-05 23:35:09 +0530

    Update socket.js

--- sync-room-ui/src/socket/socket.js
+++ sync-room-ui/src/socket/socket.js
@@ -1,11 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "";
-const SOCKET_URL_1 = import.meta.env.VITE_SOCKET_URL || "";
+const SOCKET_URL= import.meta.env.VITE_SOCKET_URL || "";
 
-
-console.log("SOCKET_URL =", SOCKET_URL);
-console.log("SOCKET_URL_1 =", SOCKET_URL_1);
 
 const socket = io(SOCKET_URL, {
   autoConnect: false,
